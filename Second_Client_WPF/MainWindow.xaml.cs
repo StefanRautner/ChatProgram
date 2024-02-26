7c0167d91ae8a8b6ae94282e206e791ad768bb86
StefanRautner <stefan.rautner06@gmail.com>
2024-02-26 11:28:19 +0100

    Initialize WPF

---
+++ Second_Client_WPF/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Second_Client_WPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
