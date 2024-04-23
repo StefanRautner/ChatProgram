9ba5d74a06e47e99b093020c9f10017764d6fb76
StefanRautner <stefan.rautner06@gmail.com>
2024-04-23 09:20:57 +0200

    Update WPF Client

--- Second_Client_WPF/obj/Debug/net8.0-windows/Chat.g.i.cs
+++ Second_Client_WPF/obj/Debug/net8.0-windows/Chat.g.i.cs
@@ -69,7 +69,7 @@ namespace Second_Client_WPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Second_Client_WPF;component/chat.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Second_Client_WPF;V1.0.0.0;component/chat.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Chat.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- Second_Client_WPF/obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ Second_Client_WPF/obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -77,7 +77,7 @@ namespace Second_Client_WPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Second_Client_WPF;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Second_Client_WPF;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
