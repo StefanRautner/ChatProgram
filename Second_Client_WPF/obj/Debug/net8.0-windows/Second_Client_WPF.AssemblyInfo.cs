7a09e1ecd01130cd39dac7c2596795038e4a7b38
StefanRautner <stefan.rautner06@gmail.com>
2024-05-24 10:19:24 +0200

    update configs

--- Second_Client_WPF/obj/Debug/net8.0-windows/Second_Client_WPF.AssemblyInfo.cs
+++ Second_Client_WPF/obj/Debug/net8.0-windows/Second_Client_WPF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Second_Client_WPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c40861676c85b383922a9ca88f3b25f4a3ccf14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c33a621f3a628839b8139b080498799cfc1ebbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Second_Client_WPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Second_Client_WPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
